fdeaf68dbe2030dcfd3c03db54a8cfb3e722e6e0
JakubFabicki <jakub.fabicki@outlook.com>
2022-06-07 19:20:33 +0200

    repair input validation

--- js/add.js
+++ js/add.js
@@ -20,43 +20,51 @@ btn.addEventListener('click', ()=>{
 });
 
 addBtn.addEventListener('click', ()=>{
-    let item = document.createElement('div');
-    item.classList.add('item');
-
-    let box = document.createElement('div');
-    box.classList.add('box');
+    if(input.value.length > 0 && input.value.length < 33){
+        let item = document.createElement('div');
+        item.classList.add('item');
     
-    let span = document.createElement('span');
-    span.innerHTML = input.value;
-    input.value = null;
-    
-    let remove = document.createElement('div');
-    remove.classList.add('remove');
+        let box = document.createElement('div');
+        box.classList.add('box');
         
-    item.appendChild(box);
-    item.appendChild(span);
-    item.appendChild(remove);
+        let span = document.createElement('span');
+        span.innerHTML = input.value;
+        input.value = null;
+        
+        let remove = document.createElement('div');
+        remove.classList.add('remove');
+            
+        item.appendChild(box);
+        item.appendChild(span);
+        item.appendChild(remove);
+        
+        tasks.appendChild(item);
+        
+        box.addEventListener('click', ()=>{
+            if(box.className === 'box fullHeart'){
+                box.classList.remove('fullHeart');
+                tasks.insertBefore(box.parentElement, null);
+                counter.innerHTML = 'COMPLETED (' + (completed.children.length-1) + ')';
     
-    tasks.appendChild(item);
+                //add to localStorage
+                localStorage.setItem(span.innerHTML, 'no');
+            }
+            else{
+                box.classList.add('fullHeart');
+                completed.insertBefore(box.parentElement, null);
+                counter.innerHTML = 'COMPLETED (' + (completed.children.length-1) + ')';
     
-    box.addEventListener('click', ()=>{
-        if(box.className === 'box fullHeart'){
-            box.classList.remove('fullHeart');
-            tasks.insertBefore(box.parentElement, null);
+                //add to localStorage
+                localStorage.setItem(span.innerHTML, 'yes');
+            }
+        });
+
+        remove.addEventListener('click', ()=>{
+            item.remove();
+            localStorage.removeItem(span.innerHTML);
             counter.innerHTML = 'COMPLETED (' + (completed.children.length-1) + ')';
-
-            //add to localStorage
-            localStorage.setItem(span.innerHTML, 'no');
-        }
-        else{
-            box.classList.add('fullHeart');
-            completed.insertBefore(box.parentElement, null);
-            counter.innerHTML = 'COMPLETED (' + (completed.children.length-1) + ')';
-
-            //add to localStorage
-            localStorage.setItem(span.innerHTML, 'yes');
-        }
-    });
-
-    localStorage.setItem(span.innerHTML, 'no');
+        });
+    
+        localStorage.setItem(span.innerHTML, 'no');
+    }
 });
